e26aaf1fe7f06d915a30a82477f72397ba9415f9
Hassan Oyeboade <oyeboadehassan@gmail.com>
2023-03-14 21:45:47 +0100

    remove hardcoded url value

--- src/pages/Url/Url.jsx
+++ src/pages/Url/Url.jsx
@@ -1,11 +1,11 @@
 import {useParams} from "react-router-dom";
-import axios from "axios";
 import {useEffect, useState} from "react";
+
+import api from '../../utils/api'
 
 const Url = () => {
     const params = useParams();
     const [error, setError] = useState('')
-    console.log(params)
     const formatUrl = (url) => {
         if (url.startsWith('http://') || url.startsWith('https://')) {
             return url
@@ -15,7 +15,7 @@ const Url = () => {
     }
 
     useEffect(() => {
-        axios.get(`https://fasturlshortenerapi.azurewebsites.net/${params.key}`).then(res => {
+        api.get(`/${params.key}`).then(res => {
             window.location.replace(formatUrl(res.data.url))
         }).catch(err => {
             setError('Invalid key')
